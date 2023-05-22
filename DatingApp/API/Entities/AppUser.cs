3f9309af8d4ecbcb5517c555cf16338bb21a6baa
Simcovich <Simcowow123@gmail.com>
2023-05-22 13:15:59 +0300

    Add Like Entity

--- DatingApp/API/Entities/AppUser.cs
+++ DatingApp/API/Entities/AppUser.cs
@@ -19,10 +19,7 @@ namespace API.Entities
         public string Country { get; set; }
         public string Interests { get; set; }
         public List<Photo> Photos { get; set; } = new();
-
-        public int GetAge()
-        {
-            return DateOfBirth.CalcuateAge();
-        }
+        public List<UserLike> LikedByUsers { get; set; }
+        public List<UserLike> LikedUsers {  get; set; }
     }
 }
